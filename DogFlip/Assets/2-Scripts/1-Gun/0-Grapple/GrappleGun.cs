fe1bece726396c1cdeec215b5c131e9d1219acff
PearJuice <pearjuice73@gmail.com>
2020-07-10 17:06:48 -0600

    Simple player

---
+++ DogFlip/Assets/2-Scripts/1-Gun/0-Grapple/GrappleGun.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrappleGun : MonoBehaviour
+{
+    public GameObject endOfGrapple;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit Hit;
+
+        if (Physics.Raycast(endOfGrapple.transform.position, new Vector3(
+            endOfGrapple.transform.localRotation.x,
+            endOfGrapple.transform.localRotation.y,
+            endOfGrapple.transform.localRotation.z), out Hit, 1))
+        {
+            if (Hit.transform.gameObject.tag == "Grappleable")
+            {
+                Debug.Log("can grapple");
+            }
+        }
+
+    }
+}
