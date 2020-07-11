f5f97b7aa143921a4b4c18a238089429f9ce5e12
PearJuice <pearjuice73@gmail.com>
2020-07-11 11:42:57 -0600

    Enemy death

--- DogFlip/Assets/2-Scripts/1-Gun/1-Handgun/handgun.cs
+++ DogFlip/Assets/2-Scripts/1-Gun/1-Handgun/handgun.cs
@@ -7,6 +7,8 @@ public class handgun : MonoBehaviour
     public GameObject player;
     Rigidbody rb;
 	ParticleSystem ps;
+
+    public int Damage;
 	public float weaponForce = 10;
 
     // Start is called before the first frame update
@@ -23,11 +25,21 @@ public class handgun : MonoBehaviour
 			ps.Emit(1);
 			rb.AddForce(-transform.forward * weaponForce, ForceMode.Impulse);
         }
+
+
+        
     }
 
     private void OnParticleCollision(GameObject other) {
 		if (other.tag == "Enemy") {
-			Debug.Log("Hit Enemy"); // todo: make enemy react
+
+            Enemy enemy = other.GetComponent<Enemy>();
+
+            if (enemy != null)
+            {
+                enemy.OnHit(Damage);
+            }
+        
 		}
 	}
 }
---
+++ DogFlip/Assets/2-Scripts/2-Enemy/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int Health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+
+    public void OnHit(int Damage)
+    {
+        Debug.Log("Hit Enemy");
+
+        Health -= Damage;
+
+        Debug.Log(Health);
+
+        if (Health <= 0)
+            Death();
+    }
+
+    void Death()
+    {
+        Destroy(this.gameObject);
+    }
+}
