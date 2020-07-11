99530a2e6f8932868a584e603532fc1eec51f5ab
BlindEagle <>
2020-07-11 06:11:14 +0300

    Made basic enemy behavior

--- DogFlip/Assets/2-Scripts/2-Enemy/AI/AI.cs
+++ DogFlip/Assets/2-Scripts/2-Enemy/AI/AI.cs
@@ -1,18 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.AI;
 
-public class AI : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class AI : MonoBehaviour {
+	NavMeshAgent agent;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public Transform target;
+	public float shootDistance;
+
+	private void Awake() {
+		agent = GetComponent<NavMeshAgent>();
+	}
+
+	private void Update() {
+		if(Physics.Raycast(transform.position, target.position-transform.position, out RaycastHit hit, shootDistance) && hit.collider.tag=="Player") {
+			agent.ResetPath();
+			// shoot at target
+		} else {
+			agent.SetDestination(target.position);
+		}
+	}
+
+	private void OnDrawGizmosSelected() {
+		Gizmos.DrawLine(transform.position, transform.position + transform.forward * shootDistance);
+	}
 }
