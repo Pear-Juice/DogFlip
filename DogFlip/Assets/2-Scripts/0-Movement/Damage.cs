3bd5a3cc15c1e0ec3908d685841bc6975b296bd4
PearJuice <pearjuice73@gmail.com>
2020-07-11 12:33:40 -0600

    Player damage

---
+++ DogFlip/Assets/2-Scripts/0-Movement/Damage.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour
+{
+    public GameObject EnemyObject;
+
+    public int Health;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Projectile")
+        {
+            Debug.Log("Shot");
+
+            TakeDamage();
+        }
+    }
+
+    void TakeDamage()
+    {
+        Enemy enemy = EnemyObject.GetComponent<Enemy>();
+        Health -= enemy.Damage;
+
+        if (Health <= 0)
+            Death();
+    }
+
+    void Death()
+    {
+        if (this.gameObject != null)
+            Destroy(this.gameObject);
+    }
+}
