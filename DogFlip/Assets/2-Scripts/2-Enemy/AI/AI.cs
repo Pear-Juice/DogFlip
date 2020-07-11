7dd22302e2bac80120fbd1228e33b24956b4a9e5
BlindEagle <>
2020-07-11 07:22:13 +0300

    AI Script

--- DogFlip/Assets/2-Scripts/2-Enemy/AI/AI.cs
+++ DogFlip/Assets/2-Scripts/2-Enemy/AI/AI.cs
@@ -7,6 +7,9 @@ public class AI : MonoBehaviour {
 	public Transform target;
 	public float shootDistance;
 
+	int poolIndex = 0;
+	public Transform bulletPoolRoot;
+
 	private void Awake() {
 		agent = GetComponent<NavMeshAgent>();
 	}
@@ -14,10 +17,19 @@ public class AI : MonoBehaviour {
 	private void Update() {
 		if(Physics.Raycast(transform.position, target.position-transform.position, out RaycastHit hit, shootDistance) && hit.collider.tag=="Player") {
 			agent.ResetPath();
-			// shoot at target
+			//transform.LookAt(target);
+			//Shoot();
 		} else {
 			agent.SetDestination(target.position);
 		}
+	}
+
+	void Shoot() { // NEEDS WORK
+		Rigidbody bullet = bulletPoolRoot.GetChild(poolIndex).GetComponent<Rigidbody>();
+		bullet.gameObject.SetActive(true);
+		bullet.position = transform.position + transform.forward * .3f;
+		bullet.velocity = transform.forward * 1f;
+		poolIndex = (int)Mathf.Repeat((float)poolIndex + 1, bulletPoolRoot.childCount);
 	}
 
 	private void OnDrawGizmosSelected() {
