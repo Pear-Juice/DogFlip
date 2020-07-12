465f5ff67e70377220cc8b6afd86414457d08aeb
Martin <38227325+mcordero96@users.noreply.github.com>
2020-07-11 21:05:57 -0500

    stapler sprite

--- DogFlip/Assets/2-Scripts/2-Enemy/enemySprite.cs
+++ DogFlip/Assets/2-Scripts/2-Enemy/enemySprite.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class spriteFollow : MonoBehaviour
+public class enemySprite : MonoBehaviour
 {
     public GameObject model;
     public SpriteRenderer spr;
     string look;
+    float angle;
 
     public Sprite u;
     public Sprite ur;
@@ -20,17 +21,29 @@ public class spriteFollow : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        look = model.GetComponent<PointToMouse>().lookDirection;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = model.transform.position + Vector3.left*0.5f;
+        transform.position = model.transform.position + Vector3.left * 0.5f;
 
-        look = model.GetComponent<PointToMouse>().lookDirection;
+        angle = Vector3.SignedAngle(Vector3.forward, -model.transform.forward , Vector3.up) - 22.5f;
 
-        switch(look){
+        if (angle >= 0 && angle < 45) look = "up-left";
+        else if (angle >= 45 && angle < 90) look = "up";
+        else if (angle >= 90 && angle < 135) look = "up-right";
+
+        else if (angle >= 135 || angle < -180) look = "right";
+
+        else if (angle >= -45 && angle < 0) look = "left";
+        else if (angle >= -90 && angle < -45) look = "down-left";
+        else if (angle >= -135 && angle < -90) look = "down";
+        else if (angle >= -180 && angle < -135) look = "down-right";
+
+        switch (look)
+        {
             case "up-left":
                 spr.sprite = ul;
                 break;
