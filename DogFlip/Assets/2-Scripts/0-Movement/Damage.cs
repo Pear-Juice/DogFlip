3a76cc0f3c948b5e4462d7347d24c355f3435181
PearJuice <pearjuice73@gmail.com>
2020-07-23 17:53:43 -0600

    Health bar and lvl3 enemies

--- DogFlip/Assets/2-Scripts/0-Movement/Damage.cs
+++ DogFlip/Assets/2-Scripts/0-Movement/Damage.cs
@@ -1,13 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Damage : MonoBehaviour
 {
 
     public GameObject DeathScreen; //reference to the death screen
+    public Slider healthBar;
+    public Image healthColor;
+    public float healthColorNumberRed;
+    public float healthColorNumberYellow;
 
     public int Health;
+
+    float healthPercent;
+
+    private void Update()
+    {
+
+        if (healthBar.value >= Health)
+        {
+            healthBar.value -= .1f;
+
+            healthColor.color = new Color(healthColorNumberRed, healthColorNumberYellow, healthColor.color.b, 1f);;
+        }
+    }
 
     public void TakeDamage(int damage)
     {
@@ -21,6 +39,13 @@ public class Damage : MonoBehaviour
         if (Health > 0)
         {
             audio.PlayHit();
+        }
+
+        healthColorNumberRed += .1f;
+
+        if (Health <= 15)
+        {
+            healthColorNumberYellow -= .1f;
         }
     }
 
